8b1596c0a0d5000ef6894cff83ad8c686e16518c
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-28 17:50:33 +0300

    Add NavigationController and push method

--- NavigationWithoutSegues/NavigationWithoutSegues/Main/ViewController.swift
+++ NavigationWithoutSegues/NavigationWithoutSegues/Main/ViewController.swift
@@ -34,7 +34,7 @@ class ViewController: UIViewController {
     @IBAction func loginButtonAction(_ sender: Any) {
         let storyboard = UIStoryboard(name: "LoginViewController", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
-        present(vc, animated: true)
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     @IBAction func termsButtonAction(_ sender: Any) {
