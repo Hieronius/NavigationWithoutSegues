11b719049f7c4ea3b03178b2fc49f9ef30a82ff5
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-28 15:53:35 +0300

    Create a new project

---
+++ NavigationWithoutSegues/NavigationWithoutSegues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationWithoutSegues
+//
+//  Created by Арсентий Халимовский on 28.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
