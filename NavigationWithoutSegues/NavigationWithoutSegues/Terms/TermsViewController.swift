8e4d78950bd770e63117772bcc489b8d10b45d22
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-28 17:54:33 +0300

    Add TermViewController

---
+++ NavigationWithoutSegues/NavigationWithoutSegues/Terms/TermsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TermsViewController.swift
+//  NavigationWithoutSegues
+//
+//  Created by Арсентий Халимовский on 28.03.2023.
+//
+
+import UIKit
+
+class TermsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
