49fa9a30177d8fe40a36d3c83386ce658ef11204
Arsenty Halimovsky <ghost12@yandex.ru>
2023-03-28 16:22:25 +0300

    Add DemoVC

---
+++ NavigationWithoutSegues/NavigationWithoutSegues/Demo/DemoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DemoViewController.swift
+//  NavigationWithoutSegues
+//
+//  Created by Арсентий Халимовский on 28.03.2023.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
